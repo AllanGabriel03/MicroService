312aa970a741af032ff54c1eaa0320a74b3a5cd6
Allan <allan.gabriel587@gmail.com>
2025-05-22 07:37:28 -0300

    subindo projeto

---
+++ ClienteServicesAlunos/ClienteServicesAlunos/src/test/java/br/com/rpires/vendas/online/ClienteServicesAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rpires.vendas.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteServicesAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
